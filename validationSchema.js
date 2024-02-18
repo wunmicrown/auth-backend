c262c66c2b522b73a2e2b3dc4635c5c3d7b4fe93
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-02-18 07:36:55 +0100

    updated

--- validationSchema.js
+++ validationSchema.js
@@ -2,33 +2,58 @@ const Joi = require("joi");
 
 
 
- const registerPayLoad=Joi.object({
+const registerPayLoad = Joi.object({
     // The payload object for create user
     firstName: Joi.string()
-    .alphanum()
-    .min(3)
-    .max(40)
-    .required(),
+        .alphanum()
+        .min(3)
+        .max(40)
+        .required(),
     lastName: Joi.string()
-    .alphanum()
-    .min(3)
-    .max(40)
-    .required(),
+        .alphanum()
+        .min(3)
+        .max(40)
+        .required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,}$')),
-    email:Joi.string().email().required(),            
+    email: Joi.string().email().required(),
 
 })
 
-// The payload object for create user login
- const loginPayLoad=Joi.object({
+// The payload object for user login
+const siginPayLoad = Joi.object({
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,}$')),
-    email:Joi.string().email().required(),            
+    email: Joi.string().email().required(),
 
 })
- const resetEmailPayLoad=Joi.object({
-    email:Joi.string().email().required(),            
+// The payload object for reset email
+const resetEmailPayLoad = Joi.object({
+    email: Joi.string().email().required(),
 
 })
 
+// The payload object for verify Otp
+const verifyOTPPayLoad = Joi.object({
+    OTP: Joi.string().required(),
+})
 
-module.exports={registerPayLoad, loginPayLoad, resetEmailPayLoad};
+// The payload object for resend Otp
+const resendOTPPayLoad = Joi.object({
+    OTP: Joi.string().required(),
+})
+const uploadFilePayLoad = Joi.object({
+    file: Joi.string().required(),
+})
+const resetPasswordlPayLoad = Joi.object({
+    email: Joi.string().email().required(),
+})
+
+
+module.exports = {
+    registerPayLoad,
+    siginPayLoad,
+    resetEmailPayLoad,
+    verifyOTPPayLoad,
+    resendOTPPayLoad,
+    uploadFilePayLoad,
+    resetPasswordlPayLoad
+};
